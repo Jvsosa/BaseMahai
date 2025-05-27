2a6b1b106e4f4823de3d0c58f72d4d4a50a4ff18
Vitor <joaovitorferreiras2002@gmail.com>
2025-05-27 15:31:54 -0300

    V5

--- resources/[ creative ]/notifyitens/fxmanifest.lua
+++ resources/[ creative ]/notifyitens/fxmanifest.lua
@@ -1,12 +1,19 @@
 
-shared_script "" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
+
+
+
+
+
 fx_version "bodacious"
 game "gta5"
+
 ui_page "web-side/index.html"
+
 client_scripts {
 	"@vrp/lib/utils.lua",
 	"client-side/*"
 }
+
 files {
 	"web-side/*"
-}              
+}                                                                      
--- resources/[ creative ]/notifyitens/web-side/app.js
+++ resources/[ creative ]/notifyitens/web-side/app.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
-	window.addEventListener("message",function(event){
-		var html = `<div class="item" style="background-image: url('nui://inventory/web-side/images/${event.data.item}.png');">
-			<div class="top">
-				<div class="itemWeight">${event.data.mode}</div>
-				<div class="itemAmount">${event.data.amount}x</div>
-			</div>
-			<div class="nameItem">${event.data.name}</div>
-		</div>`;
+    window.addEventListener("message", function(event){
+        // Mapeie os modos para as classes CSS
+        var itemClass;
+        switch (event.data.mode) {
+            case 'guardou':
+                itemClass = 'guardou';
+                break;
+            case 'equipou':
+                itemClass = 'equipou';
+                break;
+            default:
+                itemClass = 'outro'; // Classe padrão para outros modos
+                break;
+        }
 
-		$(html).fadeIn(500).appendTo("#notifyitens").delay(3000).fadeOut(500);
-	})
+        var html = `
+        <div class="item ${itemClass}" id="${event.data.mode}" style="background-color: rgba(0, 0, 0, 0.8);">
+            <div class="item-mode">${event.data.mode}</div>
+            <img src="//inventory/web-side/images//${event.data.item}.png">
+            
+            <div class="info-item">
+                <div class="nameItem">${event.data.name}</div>
+                <div class="itemRecive">${event.data.mode === 'recebeu' ? `+${event.data.amount}` : `-${event.data.amount}`}</div>
+            </div>
+        </div>`;
+
+        $(html).css({ opacity: 0 }).appendTo("#notifyitens").animate({ opacity: 1 }, 800).delay(3000).fadeOut(500);
+    });
 });
